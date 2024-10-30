556131c759871349f7aca198bcd8c90b4e8fa391
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2024-10-30 18:47:33 -0500

    Finalizacion de examen

--- Herrera Dilan Examen Progreso 1/Models/Celular.cs
+++ Herrera Dilan Examen Progreso 1/Models/Celular.cs
@@ -5,18 +5,20 @@ namespace Herrera_Dilan_Examen_Progreso_1.Models
     public class Celular
     {
         [Key]
-        [MinLength(1), MaxLength(10)]
         [Required]
+        [Range(1, 9999999999)]
         public int Id { get; set; }
+
         [MinLength(1), MaxLength(100)]
         [Required]
         [DataType(DataType.Text)]
         public string Modelo { get; set; }
-        [MinLength(1), MaxLength(10)]
+
         [Required]
+        [Range(1900, 2100)]
         public int Año { get; set; }
+
         [Required]
         public decimal Precio { get; set; }
-
     }
 }
--- Herrera Dilan Examen Progreso 1/Models/Herrera.cs
+++ Herrera Dilan Examen Progreso 1/Models/Herrera.cs
@@ -6,24 +6,29 @@ namespace Herrera_Dilan_Examen_Progreso_1.Models
     public class Herrera
     {
         [Key]
-        [MinLength(1), MaxLength(10)]
         [Required]
+        [Range(1, 9999999999)] 
         public int Cedula { get; set; }
+
         [Required]
         [Range(0, 60)]
         public double PesoKg { get; set; }
+
         [MinLength(1), MaxLength(100)]
         [Required]
         [DataType(DataType.Text)]
         public string Nombre { get; set; }
+
         [Required]
         public bool? Estudiante { get; set; }
+
         [Required]
         [DataType(DataType.DateTime)]
         public DateTime? FechaNacimiento { get; set; }
+
         public Celular? Celular { get; set; }
+
         [ForeignKey("Celular")]
         public int IdCelular { get; set; }
-
     }
 }
