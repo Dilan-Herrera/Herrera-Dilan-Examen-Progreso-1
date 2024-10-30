abc28dcf14070020e352100bb393f05e093c070d
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2024-10-30 08:32:29 -0500

    Agregar archivos de proyecto.

---
+++ Herrera Dilan Examen Progreso 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Herrera_Dilan_Examen_Progreso_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
